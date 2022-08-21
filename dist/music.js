f2dcfd93f337807cef7264d2b57edb477650df38
王子璇 <406972433@qq.com>
2022-08-22 06:03:31 +0800

    Site updated: 2022-08-22 06:03:31

---
+++ dist/music.js
@@ -0,0 +1,16 @@
+var ap = new APlayer({
+    element: document.getElementById('aplayer'),
+    narrow: false,
+	fixed: true,
+    /*自动播放，默认值为false，true为开启自动播放*/
+    autoplay: false,
+    mutex: true,
+	lrcType: 3,
+});
+	$.ajax({
+	// 你的歌单id
+	url: 'https://api.i-meto.com/meting/api?server=netease&type=playlist&id=7601335521',
+	success: function (list) {
+	ap.list.add(list);
+	}
+});
